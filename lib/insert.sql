9052524eb66519689c43ebf3cf269f5ab3aa16dd
James Gibbel <jamesgibbel@gmail.com>
2019-09-04 23:29:32 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'm', 'brown', 'bad', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'f', 'black', 'bad', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 4, 'f', 'brown', 'decent', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'f', 'brown', 'bad', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 6, 'f', 'white', 'bad', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'f', 'brown', 'good', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 8, 'm', 'tan', 'bad', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 9, 'm', 'brown', 'bad', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'm', 'brown', 'bad', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'f', 'black', 'bad', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 4, 'f', 'brown', 'decent', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'f', 'brown', 'bad', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 6, 'f', 'white', 'bad', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'f', 'brown', 'good', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 8, 'm', 'tan', 'bad', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 9, 'm', 'brown', 'bad', 1);
